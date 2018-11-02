72208429ad6facab2245436ea5d67f4779fc59cb
ozarion <cozarion@gmail.com>
2018-11-02 23:56:30 +0500

    version 1.0

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: "mongodb://orion:orion321@ds062818.mlab.com:62818/shopping_list_app"
+}
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const items = require("./routes/api/items");
+
+const app = express();
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+
+const mongoKey = require("./config/keys").MongoURI;
+
+mongoose
+  .connect(mongoKey)
+  .then(() => console.log("Mongoose Connected..."))
+  .catch(err => console.error("Error Connecting to MongoDB..."));
+
+app.use("/api/items", items);
+
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server listening on ${port}`));
