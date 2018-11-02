457fff51fd02435df8d45595456489f335545cde
ozarion <cozarion@gmail.com>
2018-11-03 00:57:20 +0500

    version 1.1

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server listening on ${port}`));
